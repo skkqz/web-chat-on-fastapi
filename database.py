86eec04c9cdad4521547bb792526d2bd9dcdffd0
Andrew <skkqw@yandex.ru>
2024-12-01 22:53:29 +0300

    Создания базового класс для моделей. Инициализация движка бд.

---
+++ database.py
@@ -0,0 +1,32 @@
+import uuid
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
+
+from config import get_db_url
+
+
+DATABASE_URL = get_db_url()
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Bace(AsyncAttrs, DeclarativeBase):
+    """
+    Абстрактный класс для моделей бд.
+    """
+
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f'{cls.__name__.lower()}s'
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4(), unique=True, nullable=False
+    )
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now())
